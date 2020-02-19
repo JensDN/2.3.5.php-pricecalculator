71fb20ed9b772fdaf96ccb100986eebfe571e3dc
Thibence <salencethibaut95@gmail.com>
2020-02-19 10:47:59 +0100

    fixing product class incl new constructor

--- Model/Product.php
+++ Model/Product.php
@@ -4,12 +4,17 @@
 class Product
 {
     private $name;
+    private $id;
     private $price;
+    private $description;
 
 
-    public function __construct(string $name)
+    public function __construct(string $name, int $id, float $price, string $description)
     {
         $this->name = $name;
+        $this->id = $id;
+        $this->price = $price;
+        $this->description = $description;
     }
 
     public function getName() : string
