1e7e4fe4d19c9493d6320f22cef12cb1e69ab6a5
Thibence <salencethibaut95@gmail.com>
2020-02-20 10:05:40 +0100

    Making login page porqué es un poco importante

---
+++ View/login.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+
+?>
+
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="UTF-8" lang="eng">
+    <title>Price Calculator Login</title>
+</head>
+<body>
+<header>
+    <h1>Login to select customer and product</h1>
+</header>
+<form method="post">
+    <input type="text" placeholder="Name + Surname">
+    <input type="text" placeholder="Password">
+    <button type="submit" name="button">Log in</button>
+</form>
+
+</body>
+</html>
+
+
